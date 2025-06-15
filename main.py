a10aae595615c6e9a59f9ba68e628cf097615621
Zachary <zach@MacBook-Pro-184.local>
2025-06-15 16:02:15 -0400

    Roman Int to Number

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
-dictionary = {'key':'jawn', 'you':'me'}
+def romanToInt(s: str):
+    mapping = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        'C': 100,
+        'D': 500,
+        "M": 1000
+    }
 
-test = dictionary['key']
+    for char in s:
+        if char not in mapping.keys():
+            raise KeyError(f'invalid char found: {char}')
 
-print(test)
 
+    s = s.replace("IV","IIII")
+    s = s.replace("IX", "VIIII")
+    s = s.replace("XL", "XXXX")
+    s = s.replace("XC", "LXXXX")
+    s = s.replace("CD", "CCCC")
+    s = s.replace("CM", "DCCCC")
 
+    soln = 0
+    for char in s:
+        soln += mapping[char]
+
+    return soln
+
+print(romanToInt('DCCXVI'))
