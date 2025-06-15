c447c12a8731dddfae2a6465c4e22261c9af1bd6
Zachary <zach@MacBook-Pro-184.local>
2025-06-15 15:45:08 -0400

    new stuff

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
-print('hello')
-print('hello again')
+class list:
+    def __init__(self):
+        self.size = 0
+        self.coins = 4
+
+    def addSize(self):
+        self.size += 1
+
+    def subtractCoins(self):
+        self.coins -= 1
+
+    def __str__(self):
+        size = str(self.size)
+        coins = str(self.coins)
+        return size + ' ' + coins
+
+obj = list()
+print(obj)
