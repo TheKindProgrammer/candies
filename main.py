893cf4dd8e50ee4b6f733392691b738e41ca2223
Zachary <zach@MacBook-Pro-184.local>
2025-06-16 07:20:56 -0400

    stuff

--- main.py
+++ main.py
@@ -1,30 +1,8 @@
-def romanToInt(s: str):
-    mapping = {
-        "I": 1,
-        "V": 5,
-        "X": 10,
-        "L": 50,
-        'C': 100,
-        'D': 500,
-        "M": 1000
-    }
-
-    for char in s:
-        if char not in mapping.keys():
-            raise KeyError(f'invalid char found: {char}')
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+         self.val = val
+         self.next = next
+def mergeTwoLists(list1, list2)
 
 
-    s = s.replace("IV","IIII")
-    s = s.replace("IX", "VIIII")
-    s = s.replace("XL", "XXXX")
-    s = s.replace("XC", "LXXXX")
-    s = s.replace("CD", "CCCC")
-    s = s.replace("CM", "DCCCC")
 
-    soln = 0
-    for char in s:
-        soln += mapping[char]
-
-    return soln
-
-print(romanToInt('DCCXVI'))
