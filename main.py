abcc6b75bcbd6c9d65a7f6cb3834feb3746b5889
Zachary <zach@MacBook-Pro-184.local>
2025-06-19 14:52:20 -0400

    data table

--- main.py
+++ main.py
@@ -1,8 +1,1 @@
-class ListNode(object):
-    def __init__(self, val=0, next=None):
-         self.val = val
-         self.next = next
-def mergeTwoLists(list1, list2)
-
-
-
+from collections import Counter
