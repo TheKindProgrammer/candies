574a58ff7b2638803ed7143c672006559b037cab
Zachary <zach@MacBook-Pro-184.local>
2025-06-22 08:57:15 -0400

    v1

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-print('test')
-print('test2')
+def fairCandySwap(aliceSizes, bobSizes):
+    alice = sum(aliceSizes)
+    bob = sum(bobSizes)
+
+    val = (alice - bob) / 2
+
+    for i in bobSizes:
+        if i + val in aliceSizes:
+            return [int(i+val),int(i)]
+
+aliceSizes = [1,1]
+bobSizes = [2,2]
+print(fairCandySwap(aliceSizes,bobSizes))
